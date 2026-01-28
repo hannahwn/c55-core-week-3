f9833606fa11fc41027dde07956f2a9c1ad36030
hannahwn <hannahwanjikunyongo@gmail.com>
2026-01-28 15:58:39 +0100

    calculates wind

--- task-4/cleanup.js
+++ task-4/cleanup.js
@@ -21,3 +21,14 @@ function adddescription(c){
   }
   return "Hot";
 }
+
+function getWindChill(celsius, windKmPerHour) {
+    let windPower = Math.pow(windKmPerHour, 0.16);
+    
+    let answer = 13.12 +
+                 0.6215 * celsius -
+                 11.37 * windPower +
+                 0.3965 * celsius * windPower;
+    
+    return answer;
+}
