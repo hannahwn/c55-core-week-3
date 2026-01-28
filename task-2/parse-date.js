81878052a4e127082a66b69addd788df3c62b74d
hannahwn <hannahwanjikunyongo@gmail.com>
2026-01-28 14:23:58 +0100

    tells format correctly

--- task-2/parse-date.js
+++ task-2/parse-date.js
@@ -1,11 +1,13 @@
 function parseDateString(dateString) {
-    let parts = dateString.split("");
+    let parts = dateString.split(" ");
 
-    let format =parts[0];
+    let format = parts[0];
     let datePart = parts[1];
 
 
     let numbers = datePart.split("-");
+
+   
 
     let first = Number(numbers[0]);
     let second = Number(numbers[1]);
@@ -15,11 +17,22 @@ function parseDateString(dateString) {
     let month;
     let year = third;
 
-    if(format ==="MDY"){
+    if(format === "MDY"){
          month = first;
          day = second;
     }
 
+    if (format === "DMY"){
+        day = first;
+        month = second;
+    }
 
-    )
+    let result = {
+        day: day,month: month, year: year
+    };
+
+
+    return result;
 }
+
+console.log(parseDateString("MDY 10-21-1983"));
