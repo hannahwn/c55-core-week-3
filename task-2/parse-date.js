df3eec6f2f57c884a651bb89ae6e8f8436299a3a
hannahwn <hannahwanjikunyongo@gmail.com>
2026-01-28 13:37:29 +0100

    split input to array

--- task-2/parse-date.js
+++ task-2/parse-date.js
@@ -1,1 +1,12 @@
-// Your code here
+function parseDateString(dateString) {
+    let parts = dateString.split("");
+
+    let format =parts[0];
+    let datePart = parts[1];
+
+
+    let numbers = datePart.split("-");
+
+
+    )
+}
